83bc726c5a1235e1bc6308522eade572b223d977
miketheinkman <mike_gibbs@rocketmail.com>
2015-01-26 21:02:23 -0600

    initial commit

---
+++ picupload.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+ 
+DATE="$(date +'%m-%d-%y')"
+STATE="$(adb get-state)"
+
+
+#picuploader caller script
+
+#poormans UDEV rules
+
+
+
+STATE="$(adb get-state)"
+
+
+
+if [ "$STATE" != "unknown" ] ; then 
+echo "device"
+	sleep 15
+
+		./picupload.sh
+
+
+
+	elif [ "$STATE" != "device" ] ; then
+
+		sleep 5
+echo "no device"
+exit 0
+
+fi
+
+echo "waiting"
+
+adb kill-server
+adb start-server
+
+adb wait-for-device
+
+
+adb get-state
+
+if [ "$STATE" != "device" ] ; then
+	
+	echo "damn" 
+
+elif [ "$STATE" = "device" ] ; then 
+
+	echo "Backing up photos"
+
+		adb pull /sdcard/DCIM/Camera ~/Desktop/pic-backup
+
+				mv ~/Desktop/pic-backup/*.mp4 ~/Desktop/video-backup
+
+			./picupload.sh
+
+	fi
+ echo "end"
+sleep 5
+
+./picupload.sh
+
+    
+exit 0
+
+
+
+
+
